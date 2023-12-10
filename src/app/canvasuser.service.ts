adbef69207e13cf09fc6263aa37cc5447005b9cb
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-12-10 15:04:21 +0100

    Created front end service part.

---
+++ src/app/canvasuser.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { CanvasUser } from "./canvasuser";
+
+@Injectable({
+    providedIn: "root"
+})
+export class EmployeeService {
+    private apiServerUrl = "";
+
+    constructor(private http: HttpClient) {}
+
+    public getCanvasUsers(): Observable<CanvasUser[]> {
+        return this.http.get<CanvasUser[]>(`${this.apiServerUrl}/canvasuser/all`);
+    }
+
+    public getCanvasUser(canvasUserId: number): Observable<CanvasUser> {
+        return this.http.get<CanvasUser>(`${this.apiServerUrl}/canvasuser/find/${canvasUserId}`);
+    }
+
+    public addCanvasUser(canvasUser: CanvasUser): Observable<CanvasUser> {
+        return this.http.post<CanvasUser>(`${this.apiServerUrl}/canvasuser/add`, canvasUser);
+    }
+
+    public updateCanvasUser(canvasUser: CanvasUser): Observable<CanvasUser> {
+        return this.http.put<CanvasUser>(`${this.apiServerUrl}/canvasuser/update`, canvasUser);
+    }
+
+    public deleteCanvasUser(canvasUserId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/canvasuser/delete/${canvasUserId}`);
+    }
+}
---
+++ src/app/canvasuser.ts
@@ -0,0 +1,10 @@
+export interface CanvasUser {
+    id: number;
+    userName: string;
+    email: string;
+    phoneNumber: string;
+    company: string;
+    jobTitle: string;
+    imageUrl: string;
+    employeeId: string;
+}
